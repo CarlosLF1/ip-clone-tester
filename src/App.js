65b0f81c57160019dd53c88e34f40795cba42b59
Carlos Ferraz <carloslf@e.email>
2022-09-26 12:39:46 +0300

    Ui component and IP components built

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import IpAddress from "./components/IpAddress";
+import Card from "./UI/Card"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card>
+      <h1>Your IP address is ...</h1>
+      <IpAddress />
+    </Card>
   );
 }
 
---
+++ src/components/IpAddress.js
@@ -0,0 +1,7 @@
+const IpAddress = () => {
+    return <div>
+    <h1>'0.0.0.0'</h1>
+</div>
+}
+
+export default IpAddress;
