ab51d3cbde7171dcd80ad56a953e68901744f776
Carlos Ferraz <carloslf@e.email>
2022-09-28 00:18:41 +0300

    attempt at global state - not working

---
+++ state/index.js
@@ -0,0 +1,8 @@
+import { Marker } from "react-leaflet";
+import { createGlobalState } from "react-hooks-global-state";
+
+const { setGlobalState, useGlobalState } = createGlobalState({
+    
+});
+
+export { useGlobalState, setGlobalState };
