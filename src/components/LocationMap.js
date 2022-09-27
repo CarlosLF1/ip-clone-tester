c356559b3620ec80e20d3c68cefacf08cfdc1a69
Carlos Ferraz <carloslf@e.email>
2022-09-27 20:09:30 +0300

    added react leaflet and detail for map

--- src/components/LocationMap.js
+++ src/components/LocationMap.js
@@ -1,12 +1,24 @@
-import Card from "../UI/Card";
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const LocationMap = (props) => {
     return (
-    <Card>
-        <img
-            src={`https://maps.googleapis.com/maps/api/staticmap?center=${props.lat},%20${props.lon}&zoom=6&size=400x400&markers=color:red%7C${props.lat},%20${props.lon}&key=AIzaSyDCXubq8XxLxR7zLM67f5lKmurcZspplPo`}
-            alt="map view" />
-        </Card>
+    <div className="leafLetContainer">
+            <MapContainer style={{ height: '100%', width: '100%' }}
+            center={[ipAddressDict?.location?.lat, ipAddressDict?.location?.lng]}
+            zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http:osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                />
+                <Marker 
+                    position={[ipAddressDict?.location?.lat, ipAddressDict?.location?.lng]}
+                >
+                    <Popup>{ipAddressDict?.location}</Popup>
+                </Marker>
+        </MapContainer>
+
+    </div>
     );
 };
 
