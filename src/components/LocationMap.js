3990f389e9e2178b36928e4abb22a4ad4b4470aa
Carlos Ferraz <carloslf@e.email>
2022-09-27 19:28:31 +0300

    Loading api info for map and card for map

--- src/components/LocationMap.js
+++ src/components/LocationMap.js
@@ -1,10 +1,13 @@
 import Card from "../UI/Card";
 
 const LocationMap = (props) => {
-    return
+    return (
     <Card>
-        <img src="`https://maps.googleapis.com/maps/api/staticmap?center=${props.lat},%20${props.lon}&zoom=6&size=400x400&markers=color:red%7C${props.lat},%20${props.lon}&key=${GCP_MAP_API_KEY}`" alt="map view" />
-    </Card>;
+        <img
+            src={`https://maps.googleapis.com/maps/api/staticmap?center=${props.lat},%20${props.lon}&zoom=6&size=400x400&markers=color:red%7C${props.lat},%20${props.lon}&key=AIzaSyDCXubq8XxLxR7zLM67f5lKmurcZspplPo`}
+            alt="map view" />
+        </Card>
+    );
 };
 
 export default LocationMap;
