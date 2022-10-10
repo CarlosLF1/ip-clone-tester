febc14b578a69f03cffc35256873e6469ac2c4ff
Oktay Bolat <oktay.bolat@gmail.com>
2022-10-10 17:26:45 +0300

    API changed

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <BrowserRouter>
       <Header />
         <Routes>
-         <Route path='/' element={<App />} />
+         <Route path='/:ipSearch' element={<App />} />
         </Routes>
       <Footer />
   </BrowserRouter>
